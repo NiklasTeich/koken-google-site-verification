7e03d4c1e1d652d56c230972140461edfb6c66c9
Niklas Teich <n.teich@teich-datensysteme.de>
2017-07-18 19:36:40 +0200

    doc update
    updated plugin class php doc

--- plugin.php
+++ plugin.php
@@ -4,7 +4,7 @@
  * Koken CMS plugin to add a Google Search Console site verification HTML tag
  * to your website instead of adding a static HTML file.
  *
- * @author NiklasTeich <niklas@millenworld.de>
+ * @author Niklas Teich <niklas@millenworld.de>
  */
 
 class NiklasTeichGoogleSiteVerification extends KokenPlugin {
